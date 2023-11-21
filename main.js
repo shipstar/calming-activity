1ff03b285fa9bffde5ab130b679f67cbacf48219
Kyle Shipley <kshipley@hellobrightline.com>
2023-11-21 10:44:22 -0500

    Remove arcade physics

--- main.js
+++ main.js
@@ -54,13 +54,6 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
-    },
-  },
   scene: {
     preload: preload,
     create: create,
